86af1b62a7d72df2fb5b8e6de2eccbc5c11127da
Marvin Kuhn <marvin.kuhn92@gmail.com>
2020-01-28 22:30:40 +0100

    added meta tags to documentation pages

--- Documentation/.vuepress/config.js
+++ Documentation/.vuepress/config.js
@@ -1,6 +1,23 @@
+const socialMeta = {
+    title: 'Neos Backups',
+    description: 'Easy backup management plugin for Neos CMS',
+    image: 'https://repository-images.githubusercontent.com/235191200/234dfe80-4129-11ea-9f1d-a938b96ec553',
+    url: 'https://breadlesscode.github.io/neos-backups'
+};
+
 module.exports = {
     title: 'Neos Backups',
     base: process.env.NODE_ENV === 'production'? '/neos-backups/': '/',
+    head: [
+        ['meta', {property: 'og:title', content: socialMeta.title}],
+        ['meta', {property: 'og:description', content: socialMeta.description}],
+        ['meta', {property: 'og:image', content: socialMeta.image}],
+        ['meta', {property: 'og:url', content: socialMeta.url}],
+        ['meta', {property: 'twitter:title', content: socialMeta.title}],
+        ['meta', {property: 'twitter:description', content: socialMeta.description}],
+        ['meta', {property: 'twitter:image', content: socialMeta.image}],
+        ['meta', {property: 'twitter:card', content: 'summary_large_image'}],
+    ],
     themeConfig: {
         nav: [
             {text: 'Github', link: 'https://github.com/breadlesscode/neos-backups'}
